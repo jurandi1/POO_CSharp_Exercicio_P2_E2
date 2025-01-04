b0bac8dce38c6c6013f3d1f0663e3f0f9e43f551
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 18:58:47 -0300

    Exercício concluído

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
-﻿internal class Program
+﻿using POO_CSharp_Exercicio_P2_E2;
+using System;
+using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        //EXERCÍCIO 02:
+        //Fazer um programa para ler os dados de um funcionário(nome,
+        //salário bruto e imposto). Em seguida, mostrar os dados do
+        //funcionário(nome e salário líquido).Em seguida, aumentar o salário
+        //do funcionário com base em uma porcentagem dada(somente o
+        //salário bruto é afetado pela porcentagem) e mostrar novamente os
+        //dados do funcionário.Use a classe projetada abaixo.
+
+        Funcionario func = new Funcionario();
+
+        Console.Write("Nome: ");
+        func.Nome = Console.ReadLine();
+        Console.Write("Salario bruto: ");
+        func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Imposto: ");
+        func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.WriteLine("Funcionário: " + func);
+
+        Console.WriteLine();
+        Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+        double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        func.AumentarSalario(porcent);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + func);
+
     }
 }
